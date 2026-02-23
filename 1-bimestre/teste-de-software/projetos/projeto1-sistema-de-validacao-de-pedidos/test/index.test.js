7a9f4a59485058d8e12d56457216daaadfba8b6b
Thiago Botelho <155117055+thiago2898@users.noreply.github.com>
2026-02-23 17:13:13 +0000

    projeto concluído

--- 1-bimestre/teste-de-software/projetos/projeto1-sistema-de-validacao-de-pedidos/test/index.test.js
+++ 1-bimestre/teste-de-software/projetos/projeto1-sistema-de-validacao-de-pedidos/test/index.test.js
@@ -1,21 +1,21 @@
 const validarPedido = require('../index')
 
-test("Validação de preço total", () => {
-    const pedido = { cliente: "Carlos", total: 150, itens: ["Produto A", "Produto B"]}
+test("Validar se o preço total do pedido é maior que 0", () => {
+    const pedido = { cliente: "Carlos", total: 150, itens: ["Produto A", "Produto B"] }
     expect(validarPedido(pedido)).toBeTruthy()
 })
 
-test("Verificando se há a propriedade cliente no pedido", () => {
-    const pedido = { cliente: "Everson", total: 20, itens: ["Produto A", "Produto B"]}
-    expect(validarPedido(pedido)).toBeTruthy()
+test("Invalidar pedido sem cliente", () => {
+    const pedido = { total: 20, itens: ["Produto A", "Produto B"] }
+    expect(validarPedido(pedido)).toBeFalsy()
 })
 
-test("Verificando se no pedido há uma lista de itens", () => {
-    const pedido = { cliente: "Enzo", total: 100, itens: ["Produto A", "Produto B"]}
-    expect(validarPedido(pedido)).toBeTruthy()
+test("Invalidar pedido sem lista", () => {
+    const pedido = { cliente: "Enzo", total: 100 }
+    expect(validarPedido(pedido)).toBeFalsy()
 })
 
-test("Validação de pedido", () => {
-    const pedido = { cliente: "Helder", total: 200, itens: ["Produto A", "Produto B"]}
-    expect(validarPedido(pedido)).toBeTruthy()
+test("Invalidar pedido", () => {
+    const pedido = { }
+    expect(validarPedido(pedido)).toBeFalsy()
 })
