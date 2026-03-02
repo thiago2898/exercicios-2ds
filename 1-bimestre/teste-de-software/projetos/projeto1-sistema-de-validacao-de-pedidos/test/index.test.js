3083539356e897cdbba0f026137c6813060c06dc
Thiago Botelho <155117055+thiago2898@users.noreply.github.com>
2026-03-02 11:05:49 +0000

    correção

--- 1-bimestre/teste-de-software/projetos/projeto1-sistema-de-validacao-de-pedidos/test/index.test.js
+++ 1-bimestre/teste-de-software/projetos/projeto1-sistema-de-validacao-de-pedidos/test/index.test.js
@@ -1,21 +1,21 @@
 const validarPedido = require('../index')
 
-test("Validar se o preço total do pedido é maior que 0", () => {
-    const pedido = { cliente: "Carlos", total: 150, itens: ["Produto A", "Produto B"] }
-    expect(validarPedido(pedido)).toBeTruthy()
+test("Pedido com total 0 deve retornar false", () => {
+    const pedido = { cliente: "Carlos", total: 0, itens: ["Produto A", "Produto B"] }
+    expect(validarPedido(pedido)).toBeFalsy()
 })
 
-test("Invalidar pedido sem cliente", () => {
+test("Pedido sem cliente deve retornar false", () => {
     const pedido = { total: 20, itens: ["Produto A", "Produto B"] }
     expect(validarPedido(pedido)).toBeFalsy()
 })
 
-test("Invalidar pedido sem lista", () => {
+test("Pedido sem itens deve retornar false", () => {
     const pedido = { cliente: "Enzo", total: 100 }
     expect(validarPedido(pedido)).toBeFalsy()
 })
 
-test("Invalidar pedido", () => {
-    const pedido = { }
-    expect(validarPedido(pedido)).toBeFalsy()
+test("Pedido válido deve retornar true", () => {
+    const pedido = {cliente: "Carlos", total: 150, itens: ["Produto A", "Produto B"]}
+    expect(validarPedido(pedido)).toBeTruthy()
 })
